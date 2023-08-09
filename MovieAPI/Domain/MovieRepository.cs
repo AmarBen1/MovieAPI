a02146df44033206847a81b86bbc2b8e96fd51a2
Amar <fap119@hotmail.com>
2023-08-09 13:32:22 +0200

    GetAllMovies implementation

--- MovieAPI/Domain/MovieRepository.cs
+++ MovieAPI/Domain/MovieRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace MovieAPI.Domain
+﻿using Microsoft.EntityFrameworkCore;
+using MovieAPI.Data;
+using MovieAPI.Interfaces;
+
+namespace MovieAPI.Domain
 {
-    public class MovieRepository
+    public class MovieRepository : IMovieRepository
     {
+        private readonly MovieDbContext _context;
+
+        public MovieRepository(MovieDbContext context) 
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Movie>> GetAllMovies()
+        {
+            return await _context.Movies
+                .Include(x=>x.Actors)
+                .Include(x=>x.Director)
+                .ToListAsync();            
+        }
     }
 }
